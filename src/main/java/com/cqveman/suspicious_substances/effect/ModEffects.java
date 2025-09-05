d0ccd2cd446fcf613822d2f1847fab439f2cfd9e
cqveman <trymemain.pb.hug@gmail.com>
2025-09-05 13:03:22 +0300

    Created ModEffects class to handle custom status effects

---
+++ src/main/java/com/cqveman/suspicious_substances/effect/ModEffects.java
@@ -0,0 +1,21 @@
+package com.cqveman.suspicious_substances.effect;
+
+import com.cqveman.suspicious_substances.SuspiciousSubstances;
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffectCategory;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.util.Identifier;
+
+public class ModEffects {
+    public static final RegistryEntry<StatusEffect> BONELESS = register("boneless", new BonelessEffect(StatusEffectCategory.NEUTRAL, 16777200));
+
+    private static RegistryEntry<StatusEffect> register(String name, StatusEffect statusEffect) {
+        return Registry.registerReference(Registries.STATUS_EFFECT, Identifier.of(SuspiciousSubstances.MOD_ID, name), statusEffect);
+    }
+
+    public static void initialize() {
+        SuspiciousSubstances.LOGGER.info("Registering Mod Effects for " + SuspiciousSubstances.MOD_ID);
+    }
+}
