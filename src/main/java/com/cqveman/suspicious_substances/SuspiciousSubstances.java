d225af1e06934084c481963589d75f9930367107
cqveman <trymemain.pb.hug@gmail.com>
2025-09-05 12:51:59 +0300

    Initial commit

---
+++ src/main/java/com/cqveman/suspicious_substances/SuspiciousSubstances.java
@@ -0,0 +1,14 @@
+package com.cqveman.suspicious_substances;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SuspiciousSubstances implements ModInitializer {
+    public static final String MOD_ID = "suspicious_substances";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/com/cqveman/suspicious_substances/client/SuspiciousSubstancesClient.java
@@ -0,0 +1,10 @@
+package com.cqveman.suspicious_substances.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SuspiciousSubstancesClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
